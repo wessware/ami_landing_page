6c5ed637729b715a392bdb808f7cab01af3c1809
wessware <stevenshisia@yahoo.com>
2024-03-30 18:57:11 +0300

    project files

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require("express")
+const expressLayouts = require('express-ejs-layouts')
+
+const app= express()
+const port = 5000 || process.env.PORT 
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//static files
+app.use(express.static('public'))
+
+//templating engine
+app.use(expressLayouts)
+app.set("layout", './layouts/main')
+app.set('view engine', 'ejs')
+
+//routes
+app.use('/', require('./server/routes/index'))
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
