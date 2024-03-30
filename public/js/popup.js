dff67edfe8a9a52c3da647126beeef83d254b53c
wessware <stevenshisia@yahoo.com>
2024-03-30 18:53:28 +0300

    public js file

---
+++ public/js/popup.js
@@ -0,0 +1,67 @@
+function showWorkPopup() {
+    var popup = document.getElementById("workPopup");
+    popup.style.display = "block";
+}
+
+function hideWorkPopup() {
+    var popup = document.getElementById("workPopup");
+    popup.style.display = "none";
+}
+
+function showServicePopup() {
+    var popup = document.getElementById("servicePopup");
+    popup.style.display = "block";
+}
+
+function hideServicePopup() {
+    var popup = document.getElementById("servicePopup");
+    popup.style.display = "none";
+}
+function showContactPopup() {
+        var popup = document.getElementById("contactPopup");
+        popup.style.display = "block";
+}
+
+function hideContactPopup() {
+        var popup = document.getElementById("contactPopup");
+        popup.style.display = "none";
+}
+        function showTwitterPopup() {
+    var popup = document.getElementById("twitterPopup");
+    popup.style.display = "block";
+}
+
+function hideTwitterPopup() {
+    var popup = document.getElementById("twitterPopup");
+    popup.style.display = "none";
+}
+
+function showFacebookPopup() {
+    var popup = document.getElementById("facebookPopup");
+    popup.style.display = "block";
+}
+
+function hideFacebookPopup() {
+    var popup = document.getElementById("facebookPopup");
+    popup.style.display = "none";
+}
+
+function showGithubPopup() {
+    var popup = document.getElementById("githubPopup");
+    popup.style.display = "block";
+}
+
+function hideGithubPopup() {
+    var popup = document.getElementById("githubPopup");
+    popup.style.display = "none";
+}
+
+function showPinterestPopup() {
+    var popup = document.getElementById("pinterestPopup");
+    popup.style.display = "block";
+}
+
+function hidePinterestPopup() {
+    var popup = document.getElementById("pinterestPopup");
+    popup.style.display = "none"; 
+}
